dea38a2c32c074812056f2e4ab3efad4255f63eb
Winfred Nyakonor <137136246+wnyakonor66@users.noreply.github.com>
2026-08-03 20:57:13 +0000

    added rate limiter

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import rateLimiter from "express-rate-limit";
+
 
 
 const app = express();
 const PORT = 3030;
+const limiter = rateLimiter({ windowMs: 15 * 60 * 1000, max: 300 });
 
-app.get("/", (req, res) => (
-    res.json("Hello")
+app.use(cors());
+app.use(express.json());
+app.use(limiter);
+
+app.get("/health", (req, res) => (
+    res.status(200).json("Running...")
 ));
 
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`)
+    console.log(`Paylink server is running on PORT ${PORT}`)
 })
