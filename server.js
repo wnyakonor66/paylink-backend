6211b101298fea8209c8ba68ebe79aced8368a1c
Winfred Nyakonor <137136246+wnyakonor66@users.noreply.github.com>
2026-08-03 20:17:20 +0000

    utils/token addedd

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.get("/", (req, res) => (
 ));
 
 
+
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT${PORT}`)
+    console.log(`Server is running on PORT ${PORT}`)
 })
---
+++ utils/token.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+
+
+//SIGN IN TOKEN
+export const signAccessToken = (user) => {
+    return jwt.sign (
+        {sub: user.id, email: user.email},
+        process.env.JWT_SECRET,
+        {expiresIn: "15m"}
+    )
+};
+
+
+//REFRESH TOKEN
+export const signRefreshToken = (user) => {
+    return jwt.sign(
+        {sub: user.id, type: "refresh"},
+        process.env.JWT_REFRESH,
+        {expiresIn: "7d"}
+    )
+};
+
+
+//VERIFY SIGN IN TOKEN
+export const verifyAccessToken = (token) => {
+    return jwt.verify(token, process.env.JWT_ACCESS_SECRET)
+};
+
+
+//VERIFY REFRESH TOKEN
+export const verifyRefreshToken = (token) => {
+    return jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+}
